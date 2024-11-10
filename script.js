97daa0f515e3e070c900d23ff8f82d256f802434
Mygameindie <poomkhrit14@gmail.com>
2024-11-10 17:26:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 // Array of JSON file paths
 const jsonFiles = [
-    'bottomunderwear1.json', 'bottomunderwear2.json', 'bottomunderwear3.json', 
-    'topunderwear1.json', 'topunderwear2.json', 'topunderwear3.json', 
-    'boxers1.json', 'boxers2.json', 'boxers3.json',
-    'socks1.json', 'socks2.json', 'socks3.json', 
-    'shoes1.json', 'shoes2.json', 'shoes3.json', 
-    'top1.json', 'top2.json', 'top3.json', 
-    'dress1.json', 'dress2.json', 'dress3.json', 
-    'pants1.json', 'pants2.json', 'pants3.json', 
-    'skirt1.json', 'skirt2.json', 'skirt3.json', 
-    'jacket1.json', 'jacket2.json', 'jacket3.json', 
-    'accessories1.json', 'accessories2.json', 'accessories3.json',
-    'sweatshirt1.json', 'sweatshirt2.json', 'sweatshirt3.json',
-    'hat1.json', 'hat2.json', 'hat3.json' // New hat categories
+    'bottomunderwear1.json', 'bottomunderwear2.json',
+    'topunderwear1.json', 'topunderwear2.json',
+    'boxers1.json', 'boxers2.json',
+    'shoes1.json', 'shoes2.json',
+    'top1.json', 'top2.json',
+    'dress1.json', 'dress2.json',
+    'pants1.json', 'pants2.json',  
+    'skirt1.json', 'skirt2.json', 
+    'jacket1.json', 'jacket2.json',
+    'accessories1.json', 'accessories2.json',
+    'sweatshirt1.json', 'sweatshirt2.json',
+    'hat1.json', 'hat2.json', 
 ];
 
 // Load each JSON file
